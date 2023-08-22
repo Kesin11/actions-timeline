da342b0f2c1782e0cad17991b5abc853390794da
Kenta Kase <kesin1202000@gmail.com>
2023-08-22 15:55:50 +0000

    Init deno

---
+++ main_test.ts
@@ -0,0 +1,6 @@
+import { assertEquals } from "https://deno.land/std@0.189.0/testing/asserts.ts";
+import { add } from "./main.ts";
+
+Deno.test(function addTest() {
+  assertEquals(add(2, 3), 5);
+});
