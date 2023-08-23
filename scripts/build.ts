66340b2a6f5bda07db9e0627f205a342f18dc42b
Kenta Kase <kesin1202000@gmail.com>
2023-08-23 15:46:17 +0000

    Bundle js by esbuild

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,14 +1,16 @@
 import { build, emptyDir } from "https://deno.land/x/dnt@0.38.1/mod.ts";
+import * as esbuild from "https://deno.land/x/esbuild@v0.19.2/mod.js"
+
+console.debug("Start dnt ...")
 
 const outDir = "./npm";
 await emptyDir(outDir);
-
 await build({
   entryPoints: ["./main.ts"],
   outDir,
   typeCheck: false,
   declaration: false,
-  scriptModule: false,
+  esModule: false,
   shims: {
     // see JS docs for overview and more options
     deno: true,
@@ -28,3 +30,20 @@ await build({
     },
   },
 });
+
+console.log("Start esbuild ...")
+const distDir = "./dist";
+await emptyDir(distDir);
+
+await esbuild.build({
+  entryPoints: ["./npm/src/main.ts"],
+  outdir: distDir,
+  bundle: true,
+  platform: "node",
+  target: "node16",
+  format: "cjs",
+  minify: true,
+  sourcemap: true,
+}).finally(() => {
+  esbuild.stop()
+})
