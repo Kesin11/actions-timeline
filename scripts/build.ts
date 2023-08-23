904417c5848c0ccb4065c705e81e990c08ff4658
Kenta Kase <kesin1202000@gmail.com>
2023-08-23 16:22:36 +0000

    Import esbuild types

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,4 +1,5 @@
 import { build, emptyDir } from "https://deno.land/x/dnt@0.38.1/mod.ts";
+// @deno-types="https://deno.land/x/esbuild@v0.19.2/mod.d.ts"
 import * as esbuild from "https://deno.land/x/esbuild@v0.19.2/mod.js"
 
 console.debug("Start dnt ...")
