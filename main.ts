841171ce86f76256df795963a0b84fefa583179c
Kenta Kase <kesin1202000@gmail.com>
2023-08-25 16:21:43 +0000

    Update sample gantt

--- main.ts
+++ main.ts
@@ -14,17 +14,21 @@ const main = async () => {
   const gantt = `
 \`\`\`mermaid
 gantt
-    title job gantt POC
+    title ジョブのガントPOC
     dateFormat  HH:mm:ss
     axisFormat  %H:%M:%S
     section Job1
-    actions/checkout@v3 :Job1-1, 01:00:00, 30s
-    actions/setup-node@v2 :Job1-2, after Job1-1, 20s
-    npm run build :Job1-3, after Job1-2, 10s
+      actions/checkout@v3 :done, Job1-1, 00:00:00, 30s
+      actions/setup-node@v2 :active, Job1-2, after Job1-1, 20s
+      npm run build :crit, done, Job1-3, after Job1-2, 10s
     section Job2
-    actions/checkout@v3 :Job2-1, 01:00:00, 10s
-    actions/download-artifact :Job2-2, after Job2-1, 20s
-    actions/cache :Job2-3, after Job2-2, 5s
+      actions/checkout@v3 :Job2-1, 00:00:00, 10s
+      actions/upload-artifact :Job2-2, after Job2-1, 20s
+      actions/cache :Job2-3, after Job2-2, 5s
+    section Job3
+    actions/checkout@v3 :Job3-1, 00:01:10, 10s
+    actions/download-artifact :Job3-2, after Job3-1, 20s
+    build :crit, Job3-3, after Job3-2, 10s
 \`\`\`
   `
   await summary.addRaw(gantt).write()
