868fe82b16cfa1ae6977c7e787ce664b7e4039c6
Kenta Kase <kesin1202000@gmail.com>
2024-01-13 13:58:12 +0000

    deno task bundle

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,18 @@
+// npm/src/_dnt.polyfills.ts
+if (!Object.hasOwn) {
+  Object.defineProperty(Object, "hasOwn", {
+    value: function(object, property) {
+      if (object == null) {
+        throw new TypeError("Cannot convert undefined or null to object");
+      }
+      return Object.prototype.hasOwnProperty.call(Object(object), property);
+    },
+    configurable: true,
+    enumerable: false,
+    writable: true
+  });
+}
+
 // npm/src/main.ts
 var main = async () => {
 };
